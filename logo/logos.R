7dfad46a80ae4458305624c1f6f70c46d8e10256
Deepak Tanwar <dktanwar@hotmail.com>
2019-09-08 18:49:29 +0200

    poster for BC2

--- logo/logos.R
+++ logo/logos.R
@@ -7,4 +7,4 @@ cols <- RColorBrewer::brewer.pal(n = 9, name = "Set1")
 sticker(subplot = imgurl, package="shortRNA", p_size=8, p_color = "black", p_family = "Arial", 
         url = "mansuylab/shortRNA",u_color = cols[1], u_family = "Arial",
         s_x=1, s_y=0.8, s_width=0.6, s_height=0.3, h_size = 1.5,
-        filename="baseplot.png", h_fill = "white", h_color = cols[2], spotlight = T, u_size = 2)
+        filename="baseplot.png", h_fill = "white", h_color = cols[2], spotlight = F, u_size = 2)
