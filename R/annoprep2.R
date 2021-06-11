fc6b53056b44d09186bea46e2d402b85ec6cab11
Deepak Tanwar <dktanwar@hotmail.com>
2021-06-11 15:06:01 +0200

    annotation functions written and tested for mouse

--- R/annoprep2.R
+++ R/annoprep2.R
@@ -17,6 +17,8 @@
 #' @param resolveSplicing The biotypes (in the `ensdb`) for which to resolve
 #' splicing. If NULL, will resolve all but the low-priority (i.e. long RNA)
 #' transcripts.
+#' @param rules Rules for reads assignment
+#' @param tRNAEnsembleRemove removes the tRNA annotations from Ensembl database
 #' @param ... passed to `Rsubread::buildindex`
 #'
 #' @return Produces a Rsubread index and returns a GRanglesList of features
@@ -26,7 +28,10 @@
 #' @importFrom Rsubread buildIndex
 prepareAnnotation <- function(ensdb, genome = NULL, output_dir = "",
                               extra.gr = list(), extra.seqs = NULL,
-                              resolveSplicing = NULL, ...) {
+                              resolveSplicing = NULL,
+                              rules = defaultAssignRules(),
+                              tRNAEnsembleRemove = TRUE,
+                              ...) {
 
   # Loading libraries early in case they are not available,
   # program will fail early
@@ -39,7 +44,7 @@ prepareAnnotation <- function(ensdb, genome = NULL, output_dir = "",
 
   # If splicing is not to be resolved
   if (is.null(resolveSplicing)) {
-    p <- defaultAssignRules()$highPriorityTypes
+    p <- rules$priorities
     resolveSplicing <- names(p)[p >= 0]
   }
 
@@ -79,51 +84,91 @@ prepareAnnotation <- function(ensdb, genome = NULL, output_dir = "",
 
   if (!is.null(resolveSplicing)) {
     gs <- genes(ensdb,
-      filter = ~ !(tx_biotype %in% resolveSplicing),
+      filter = ~ tx_biotype != resolveSplicing,
       columns = c("gene_id", "gene_biotype", "symbol")
     )
     gs$tx_id <- gs$symbol
     gs$tx_biotype <- gs$gene_biotype
     gs$gene_id <- gs$gene_biotype <- NULL
-    anofilter <- ~ tx_biotype %in% resolveSplicing
+    anofilter <- ~ tx_biotype == resolveSplicing
   } else {
     anofilter <- NULL
   }
+
   tx <- exonsBy(ensdb,
     filter = anofilter,
     columns = c("tx_id", "tx_biotype", "symbol")
   )
-  if (!is.null(resolveSplicing)) tx <- c(tx, split(gs, gs$tx_id))
 
-  if (!is.null(extra.seqs)) tx <- c(tx, split(gr, gr$tx_id))
+  tx <- unlist(tx)
+
+  if (tRNAEnsembleRemove) {
+    tx <- tx[grep("tRNA", tx$tx_biotype, invert = T)]
+  }
+
+  if (!is.null(resolveSplicing)) tx <- c(tx, gs)
+
+  if (!is.null(extra.seqs)) tx <- c(tx, gr)
+
+  tx <- tx[, c("tx_id", "tx_biotype", "symbol")]
+  colnames(mcols(tx))[colnames(mcols(tx)) == "tx_biotype"] <- "tx_type"
 
   if (length(extra.gr) > 0) {
-    extra.gr <- lapply(names(extra.gr), FUN = function(x) {
+    extra.gr1 <- lapply(names(extra.gr), FUN = function(x) {
       gr <- extra.gr[[x]]
+      seqlevelsStyle(gr) <- "ensembl"
+
       if (is(gr, "GRangesList")) {
         stopifnot(!is.null(names(gr)))
-        if (!("tx_biotype" %in% colnames(gr@unlistData))) {
+        if (!("tx_type" %in% colnames(gr@unlistData))) {
           gr@unlistData$tx_biotype <- x
         }
         if (!("symbol" %in% colnames(gr@unlistData))) {
           gr@unlistData$symbol <- gr@unlistData$tx_id
         }
-        gr@unlistData <- gr@unlistData[, c("tx_id", "tx_biotype", "symbol")]
+        gr@unlistData <- gr@unlistData[, c("tx_id", "tx_type", "symbol")]
       } else {
         stopifnot("tx_id" %in% colnames(mcols(gr)))
-        if (!("tx_biotype" %in% colnames(mcols(gr)))) mcols(gr)$tx_biotype <- x
+        if (!("tx_type" %in% colnames(mcols(gr)))) mcols(gr)$tx_biotype <- x
         if (!("symbol" %in% colnames(mcols(gr)))) mcols(gr)$symbol <- x
         if (!is.null(gr$type) && any(gr$type == "exon")) gr <- gr$exon
-        mcols(gr) <- mcols(gr)[, c("tx_id", "tx_biotype", "symbol")]
-        gr <- split(gr, gr$tx_id)
+        mcols(gr) <- mcols(gr)[, c("tx_id", "tx_type", "symbol")]
+        # gr <- split(gr, gr$tx_id)
       }
-      gr
+      return(gr)
     })
 
-
-    tx <- c(tx, do.call(c, extra.gr))
+    tx2 <- tx[!overlapsAny(tx, do.call(c, extra.gr1))]
+    tx <- c(tx2, do.call(c, extra.gr1))
     anno.out <- file.path(output_dir, "features.rds")
   }
+
+  # tx[grep(pattern = "let-7f-1|Mirlet7f-1", x = tx$symbol)]
+
+  colnames(mcols(tx))[colnames(mcols(tx)) == "tx_type"] <- "tx_biotype"
+
+  names(tx) <- NULL
+
+  tx$symbol1 <- NA
+  tx$symbol1[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")] <- paste(
+    tx$symbol[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")],
+    tx$tx_id[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")],
+    sep = "_"
+  )
+
+  tx$tx_id1 <- NA
+
+  tx$tx_id1[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")] <-
+    tx$symbol[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")]
+  tx$tx_id1[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")] <-
+    gsub("Mir", "miR-", tx$tx_id1[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")])
+
+  tx$symbol[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")] <-
+    tx$symbol1[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")]
+  tx$tx_id[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")] <-
+    tx$tx_id1[tx$tx_biotype == "miRNA" & startsWith(tx$symbol, "Mir")]
+
+  tx <- tx[, c("tx_id", "tx_biotype", "symbol")]
 
   saveRDS(tx, file = anno.out)
   message("Features saved in \n", anno.out)
@@ -183,91 +228,432 @@ prepareAnnotation <- function(ensdb, genome = NULL, output_dir = "",
   return(tx)
 }
 
-# 
-# library(AnnotationHub)
-# ah <- AnnotationHub()
-# ensdb <- rev(query(ah, "GRCm38", "Ensdb"))[[1]]
-# 
-# library(Biostrings)
-# tRNA <- readDNAStringSet("../../ssc/annotation/mm10-mature-tRNAs.fa")
-# names(tRNA) <- trimws(gsub(pattern = ".*_|\\(.*", replacement = "", x = names(tRNA)))
 
-# library(rtracklayer)
-# piRNA <- import("../extdata/piRNA_precursors_zamore_mm10.gtf")
-# piRNA <- piRNA[piRNA$type == "transcript"]
-# colnames(mcols(piRNA))[5:7] <- c("symbol", "tx_id", "tx-type")
 
-# rRNA <- readDNAStringSet("../../shortRNA_reports/sperm_gapp2014/annotation/Mus_musculus/rRNAdb/rRNA.fa")
-# names(rRNA) <- gsub(pattern = ".*\\(|\\).*|.*Y[0-9] ", replacement = "", x = names(rRNA))
+#' Obtain miRNA GRanges from mirBase
+#'
+#' @param sp 3 alphabet species code. Please check `data(species)`
+#'
+#' @return a list of GRanges annotation of miRNAs, miRNAs DNAStringSet, 
+#' and miRNA hairpins miRNAs DNAStringSet
+#' @export
+#'
+#' @examples
+#' mir_mouse <- getmiRNA(sp = "mmu")
+#' mir_human <- getmiRNA(sp = "hsa")
+getmiRNA <- function(sp = "mmu") {
+  library(rtracklayer)
+  library(plyr)
+
+  sp <- tolower(sp)
+  link <- paste0("ftp://mirbase.org/pub/mirbase/CURRENT/genomes/", sp, ".gff3")
+  gr <- rtracklayer::import(link)
+
+  gr$transcript_id <- gr$Name
+  idm <- gr$Name
+  names(idm) <- gr$ID
+  gr$gene_id <- idm[gr$Derives_from]
+  w <- which(is.na(gr$gene_id))
+  gr$gene_id[w] <- gr$Name[w]
+  gr$transcript_type <- gr$type
+  gr$transcript_type <- as.character(gr$transcript_type)
+  gr$transcript_type[gr$transcript_type == "miRNA_primary_transcript"] <-
+    "miRNA_precursor"
+
+  gr$transcript_id <- gsub(
+    pattern = paste0(sp, "-"),
+    replacement = "",
+    x = gr$transcript_id
+  )
+  gr$gene_id <- gsub(
+    pattern = paste0(sp, "-"),
+    replacement = "",
+    x = gr$gene_id
+  )
+
+
+  df <- data.frame(gr[, c("transcript_id", "gene_id", "ID", "transcript_type")])
+  df_sp <- split(df, df$gene_id)
+
+  gtf <- GRanges(
+    ldply(
+      lapply(df_sp, function(x) {
+        id <- x$ID[x$transcript_type == "miRNA_precursor"]
+        x$symbol <- paste(id, x$gene_id, sep = "_")
+        return(x)
+      }),
+      .id = NULL
+    )
+  )
+
+
+  gtf$transcript_id[gtf$transcript_type == "miRNA_precursor"] <- gtf$ID[gtf$transcript_type == "miRNA_precursor"]
+
+  gtf$tx_id <- gtf$transcript_id
+  gtf$tx_type <- gtf$transcript_type
+
+  gtf <- gtf[, c("tx_id", "symbol", "tx_type")]
+
+  miRNA <- import("ftp://mirbase.org/pub/mirbase/CURRENT/mature.fa.gz",
+    format = "fasta"
+  )
+  miRNA <- miRNA[grep(pattern = paste0("^", sp), x = names(miRNA))]
+  names(miRNA) <- sapply(names(miRNA), function(x) {
+    gsub(
+      pattern = paste0(sp, "-"),
+      replacement = "",
+      x = strsplit(x, " ")[[1]][1]
+    )
+  })
+
+  miRNA_h <- import("ftp://mirbase.org/pub/mirbase/CURRENT/hairpin.fa.gz",
+    format = "fasta"
+  )
+  miRNA_h <- miRNA_h[grep(pattern = paste0("^", sp), x = names(miRNA_h))]
+  names(miRNA_h) <- sapply(names(miRNA_h), function(x) strsplit(x, " ")[[1]][2])
+
+  return(list(gtf = gtf, mature = miRNA, hairpin = miRNA_h))
+}
+
+
+#' Cluster miRNAs located close to each other
+#'
+#' @param gr GRanges of miRNAs
+#' @param minGap minimum gap between the miRNAs for grouping
+#'
+#' @return A GRanges object
+#' @export
+#'
+#' @examples
+miRNAcluster <- function(gr, minGap = 10000) {
+  library(GenomicRanges)
+  rd <- reduce(gr, min.gapwidth = minGap)
+  hits <- findOverlaps(rd, gr)
+  idx1 <- subjectHits(hits)
+  idx2 <- queryHits(hits)
+  r1 <- data.frame(rd[idx2])
+  r1$cluster <- paste0("miRNAcluster_", r1$seqnames, ":", r1$start, "-", r1$end, r1$strand)
+  values <- data.frame(gr[idx1])
+
+  df <- GRanges(data.frame(values, cluster = r1$cluster))
+  return(df)
+}
+
+
+
+#' Obtain mitiochondrial tRNAs from mitotRNA batabase
+#'
+#' @param sp Scientific name of species
+#' @param addCCA Whether to add CCA modifications to sequences or not. 
+#' Default: TRUE
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getMttRNA <- function(sp = "Mus musculus", addCCA = TRUE) {
+  library(tRNAdbImport)
+  library(plyr)
+  tab <- data.frame(
+    import.mttRNAdb(organism = sp)
+  )[, c("tRNA_length", "tRNA_type", "tRNA_anticodon", "tRNA_seq")]
+
+  tab <- tab[!duplicated(tab), ]
+
+  tab$tRNA_type <- gsub("[0-9]", "", tab$tRNA_type)
+
+  tab_sp <- split(tab, tab$tRNA_type)
+
+  tab <- ldply(
+    lapply(tab_sp, function(x) {
+      sp_x <- split(x, x$tRNA_anticodon)
+      a <- ldply(lapply(sp_x, function(y) {
+        if (nrow(y) > 1) {
+          y$n <- 1:table(x$tRNA_anticodon)
+        } else {
+          y$n <- 1
+        }
+        y
+      }), .id = NULL)
+
+      a$name <- paste("mt_tRNA",
+        x$tRNA_type,
+        x$tRNA_anticodon,
+        a$n,
+        1,
+        sep = "-"
+      )
+      a[, !colnames(a) %in% "n"]
+    }),
+    .id = NULL
+  )
+
+  if ("His" %in% tab$tRNA_type) {
+    his <- tab$tRNA_seq[tab$tRNA_type == "His"]
+    his <- sapply(his, function(x) ifelse(test = startsWith(x, "G"), yes = x, no = paste0("G", x)))
+    tab$tRNA_seq[tab$tRNA_type == "His"] <- his
+  }
+
+  mt <- tab$tRNA_seq
+  names(mt) <- tab$name
+
+  if (addCCA) {
+    mt_CCA <- paste0(mt, "CCA")
+    names(mt_CCA) <- paste(names(mt), "CCA", sep = "_")
+    mt <- c(mt, mt_CCA)
+  }
+
+  return(DNAStringSet(mt))
+}
+
+
+
+#' Obtain tRNA sequences from the GttRNA database
+#'
+#' @param sp Species for which tRNA sequences to be obtained. Currently 
+#' available for mm10, mm39, hg19, and hg38
+#' @param mt Whether to also download and include mitochondrial tRNAs from
+#' mitotRNA database. Please see also `getMttRNA`
+#' @param addCCA Whether to add CCA modifications to sequences or not. 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gettRNA <- function(sp = "mm10", mt = TRUE, addCCA = TRUE) {
+  if (!sp %in% c("hg19", "hg38", "mm10", "mm39")) {
+    stop("Currently supported species are: hg19, hg38, mm10, mm39")
+  }
+
+  library(Biostrings)
+  url <- "http://gtrnadb.ucsc.edu/genomes/eukaryota/"
+  mt_sp <- NULL
+
+  if (sp == "hg19") {
+    url <- paste0(url, "Hsapi19/hg19-tRNAs.fa")
+    mt_sp <- "Homo sapiens"
+  } else if (sp == "hg38") {
+    url <- paste0(url, "Hsapi38/hg38-tRNAs.fa")
+    mt_sp <- "Homo sapiens"
+  } else if (sp == "mm10") {
+    url <- paste0(url, "Mmusc10/mm10-tRNAs.fa")
+    mt_sp <- "Mus musculus"
+  } else if (sp == "mm39") {
+    url <- paste0(url, "Mmusc39/mm39-tRNAs.fa")
+    mt_sp <- "Mus musculus"
+  }
+
+  trna <- readDNAStringSet(url)
+  names(trna) <- trimws(gsub(pattern = ".*_|\\(.*", replacement = "", x = names(trna)))
+
+  if (any(grepl("His", names(trna)))) {
+    trna <- as.character(trna)
+    his <- trna[grepl("His", names(trna))]
+    his <- sapply(his, function(x) ifelse(test = startsWith(x, "G"), yes = x, no = paste0("G", x)))
+    trna[names(his)] <- his
+    trna <- DNAStringSet(trna)
+  }
+
+  if (addCCA) {
+    trna_CCA <- paste0(as.character(trna), "CCA")
+    names(trna_CCA) <- paste(names(trna), "CCA", sep = "_")
+    trna <- c(trna, DNAStringSet(trna_CCA))
+  }
+
+  if (mt) {
+    mt <- getMttRNA(sp = mt_sp, addCCA = addCCA)
+    trna <- c(trna, mt)
+  }
+  return(trna)
+}
+
+
+#' Convert a RNAStringSet to DNAStringSet
+#'
+#' @param rss RNAStringSet
+#'
+#' @return
+#' @export
+#'
+#' @examples
+convertRSStoDSS <- function(rss) {
+  rss <- as.character(rss)
+  rss <- sapply(rss, function(x) gsub("U", "T", x))
+  DNAStringSet(rss)
+}
+
+
+#' Obtain rRNAs from SILVA database
+#'
+#' @param sp Scientific names of the species
+#' @param release Which release should be used from 128, 132 and 138.1
+#'
+#' @return DNAStringSet of rRNAs
+#' @export
+#'
+#' @examples
+getrRNA <- function(sp = "Mus musculus", release = "138.1") {
+  if (!release %in% c("128", "132", "138.1")) {
+    stop("Currently following releases are supported: 128, 132, 138.1 \n")
+  }
+
+  library(Biostrings)
+
+  url_files <- paste0("https://ftp.arb-silva.de/release_", release, "/Exports/")
+  files <- listFilesFTP(url_files)
+
+  ss_file <- grep(
+    pattern = "SSURef_N[R|r]99_tax_silva.fasta.gz$",
+    x = files,
+    value = TRUE
+  )
+
+  if (length(ss_file) == 0) {
+    ss_file <- grep(
+      pattern = "SSURef_tax_silva.fasta.gz$",
+      x = files,
+      value = TRUE
+    )
+  }
+
+  ss <- readRNAStringSet(ss_file)
+  ss <- ss[grepl(pattern = sp, x = names(ss))]
+  ss <- convertRSStoDSS(ss)
+  names(ss) <- gsub(pattern = "\\..*", replacement = "", x = names(ss))
+
+  ls_file <- grep(
+    pattern = "LSURef_N[R|r]99_tax_silva.fasta.gz$",
+    x = files,
+    value = TRUE
+  )
+  if (length(ls_file) == 0) {
+    ls_file <- grep(
+      pattern = "LSURef_tax_silva.fasta.gz$",
+      x = files,
+      value = TRUE
+    )
+  }
+
+  ls <- readRNAStringSet(ls_file)
+  ls <- ls[grepl(pattern = sp, x = names(ls))]
+  ls <- convertRSStoDSS(ls)
+  names(ls) <- gsub(pattern = "\\..*", replacement = "", x = names(ls))
+
+  if (release != "128") {
+    url_meta <- paste0(url_files, "full_metadata/")
+    library(data.table)
+    meta <- listFilesFTP(url_meta)
+
+    ssm <- grep(
+      pattern = "SSURef_N[R|r]99.full_metadata.gz$",
+      x = meta,
+      value = TRUE
+    )
+
+    if (length(ssm) == 0) {
+      ssm <- grep(
+        pattern = "SSURef.full_metadata.gz$",
+        x = meta,
+        value = TRUE
+      )
+    }
+
+    ssm <- fread(input = ssm)
+    ssm <- data.frame(ssm[ssm$acc %in% names(ss), ])
+    rownames(ssm) <- ssm$acc
+
+
+    lsm <- grep(
+      pattern = "LSURef_N[R|r]99.full_metadata.gz$",
+      x = meta,
+      value = TRUE
+    )
+
+    if (length(lsm) == 0) {
+      lsm <- grep(
+        pattern = "LSURef.full_metadata.gz$",
+        x = meta,
+        value = TRUE
+      )
+    }
+
+    lsm <- fread(input = lsm)
+    lsm <- data.frame(lsm[lsm$acc %in% names(ls), ])
+    rownames(lsm) <- lsm$acc
+
+    names(ss) <- paste0("SSU-", names(ss), "(", ssm[names(ss), "product"], ")")
+    names(ls) <- paste0("LSU-", names(ls), "(", lsm[names(ls), "product"], ")")
+  } else {
+    names(ss) <- paste0("SSU-", names(ss))
+    names(ls) <- paste0("LSU-", names(ls))
+  }
+
+  res <- c(ss, ls)
+  names(res) <- gsub(pattern = "\\()", replacement = "", x = names(res))
+
+  return(res)
+}
+
+
+#' Obtain databases for species (currently supported for mouse only)
+#'
+#' @param species GRCm38 or GRCm39
+#'
+#' @return A list of databases (DNAstringSet or GRanges)
+#'
+#' @export
+#'
+#' @examples
+getDB <- function(species = "mmu", genomeVersion = "GRCm38",
+                  tRNA_addCCA = TRUE, tRNA_includeMt = TRUE,
+                  rRNA_release = "138.1") {
+  if (species == "mmu") {
+    # EnsDb
+    library(AnnotationHub)
+    ah <- AnnotationHub()
+    ensdb <- rev(query(ah, genomeVersion, "Ensdb"))[[1]]
+
+    # miRNA
+    miRNA <- getmiRNA(sp = species)
+
+    # tRNA
+    if (genomeVersion == "GRCm38") {
+      tRNA <- gettRNA(sp = "mm10", addCCA = tRNA_addCCA, mt = tRNA_includeMt)
+    } else if (genomeVersion == "GRCm39") {
+      tRNA <- gettRNA(sp = "mm39", addCCA = tRNA_addCCA, mt = tRNA_includeMt)
+    }
+
+    # piRNA precursors
+    data("piRNA_mmu")
+    piRNA <- piRNA_mmu
+    piRNA <- piRNA[piRNA$type == "transcript"]
+    colnames(mcols(piRNA))[5:7] <- c("symbol", "tx_id", "tx_type")
+    piRNA <- piRNA[, c("symbol", "tx_id", "tx_type")]
+
+    # rRNA
+    rRNA <- getrRNA(sp = "Mus musculus", release = rRNA_release)
+
+    db <- list(
+      ensdb = ensdb,
+      miRNA_GR = miRNA$gtf,
+      tRNA_fa = tRNA,
+      piRNA_GR = piRNA,
+      rRNA_fa = rRNA
+    )
+    return(db)
+  }
+}
+
+
+# db_mmu <- getDB()
 
 # mm10_annoprep <- prepareAnnotation(
-#  ensdb = ensdb,
-# output_dir = "../../shortRNA_reports/sperm_gapp2014/shortRNA/genome1",
-# extra.gr = list(piRNA = piRNA),
-# extra.seqs = list(tRNA = tRNA, rRNA = rRNA),
-# resolveSplicing = FALSE
-# )
-
-
-# test1 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   genome = "/mnt/IM/tmp/ensdb/reference.fa",
-#   output_dir = "/mnt/IM/tmp/ensdb/test1",
-#   extra.seqs = list(tRNA = tRNA),
-#   extra.gr = list(piRNA = piRNA)
-# )
-#
-#
-# test2 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   genome = "/mnt/IM/tmp/ensdb/reference.2bit",
-#   output_dir = "/mnt/IM/tmp/ensdb/test2",
-#   extra.seqs = list(tRNA = tRNA),
-#   extra.gr = list(piRNA = piRNA)
-# )
-#
-# test3 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   genome = "/mnt/IM/tmp/ensdb/reference.2bit",
-#   output_dir = "/mnt/IM/tmp/ensdb/test3",
-#   extra.seqs = list(tRNA = tRNA)
-# )
-#
-# test4 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   genome = "/mnt/IM/tmp/ensdb/reference.2bit",
-#   output_dir = "/mnt/IM/tmp/ensdb/test4",
-#   extra.gr = list(piRNA = piRNA)
-# )
-#
-# test5 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   genome = "/mnt/IM/tmp/ensdb/reference.2bit",
-#   output_dir = "/mnt/IM/tmp/ensdb/test5"
-# )
-#
-#
-# test6 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   output_dir = "/mnt/IM/tmp/ensdb/test6"
-# )
-#
-#
-# test7 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   output_dir = "/mnt/IM/tmp/ensdb/test7",
-#   extra.seqs = list(tRNA = tRNA)
-# )
-#
-# test8 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   output_dir = "/mnt/IM/tmp/ensdb/test8",
-#   extra.gr = list(piRNA = piRNA)
-# )
-#
-# test9 <- prepareAnnotation(
-#   ensdb = ensdb,
-#   output_dir = "/mnt/IM/tmp/ensdb/test9",
-#   extra.seqs = list(tRNA = tRNA),
-#   extra.gr = list(piRNA = piRNA)
+#   ensdb = db_mmu$ensdb,
+#   genome = "/mnt/IM/reference/genome/gencode/fasta/GRCm38.p5.genome.fa",
+#   output_dir = "./",
+#   extra.gr = list(piRNA = db_mmu$piRNA_GR, miRNA = db_mmu$miRNA_GR),
+#   extra.seqs = list(tRNA = db_mmu$tRNA_fa, rRNA = db_mmu$rRNA_fa),
+#   resolveSplicing = NULL,
+#   rules = defaultAssignRules(),
+#   tRNAEnsembleRemove = TRUE
 # )
