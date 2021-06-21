b2b5b14d9045eac8b5db02bbbbff639e67381eb4
Deepak Tanwar <dktanwar@hotmail.com>
2021-06-21 22:26:26 +0200

    some changes to factorList

--- R/factorList.R
+++ R/factorList.R
@@ -179,9 +179,6 @@ addReadsToTree <- function(fL, mappedFeaturesDF, featuresCol = "transcript_id", 
   # Parallel processing of apply functions
   plan(multisession)
 
-  # # pathString to factorList
-  fl <- fl
-  # 
   # Features per sequence
   features <- mappedFeaturesDF[, featuresCol]
   names(features) <- mappedFeaturesDF[, readsCol]
@@ -211,16 +208,16 @@ addReadsToTree <- function(fL, mappedFeaturesDF, featuresCol = "transcript_id", 
 
   # For sequences overlapping with more than 1 feature
   fpr_fl_m <- fpr_fl[lengths(fpr_fl) > 1]
-  fli <- IntegerList(fl)
+  fli <- IntegerList(fL)
   fpr_fl_m_o <- future_lapply(head(fpr_fl_m), FUN = function(x) longestOrderedOverlap(fli[x]))
   rm(fli)
 
   fpr_fl_m_o1 <- splitAsList(
-    x = factor(as.numeric(unlist(fpr_fl_m_o)), levels = seq_along(levels(fl[[1]])), labels = levels(fl[[1]])),
+    x = factor(as.numeric(unlist(fpr_fl_m_o)), levels = seq_along(levels(fl[[1]])), labels = levels(fL[[1]])),
     f = rep(names(fpr_fl_m_o), lengths(fpr_fl_m_o))
   )
   
-  fpr_fl_m_o2 <- FactorList(as.list(as.character(paste(head(fpr_fl_m), collapse = "/"))))
+  fpr_fl_m_o2 <- FactorList(as.list(as.character(paste(fpr_fl_m, collapse = "/"))))
   
   fpr_fl_m_of <- mergeAtomicLists(head(fpr_fl_m_o1), head(fpr_fl_m_o2))
   names(fpr_fl_m_of) <- names(fpr_fl_m_o1)
@@ -230,25 +227,18 @@ addReadsToTree <- function(fL, mappedFeaturesDF, featuresCol = "transcript_id", 
   # Reads not assigned to any features
   fpr_fl_n <- fpr_fl[lengths(fpr_fl) == 0]
   fpr_fl_n <- .addNames2fL(fpr_fl_n)
-  
+  fpr_fl_n <- as.character(unlist(fpr_fl_n))
   n <- length(fpr_fl_n)
+  fpr_fl_n <- splitAsList(as.character(unlist(fpr_fl_n)), seq_len(n))
   
-  fpr_fl_n2 <- as.character(fpr_fl_s_o[[1]][1])
+  x1 <- splitAsList(rep(factor(as.character(fpr_fl_m_of[[1]][1])), n), seq_len(n))
+  x2 <- splitAsList(rep(factor("unassigned"), n), seq_len(n))
+  x <- mergeAtomicLists(x1, x2)
   
-  
-  
-  a <- FactorList(as.list(LETTERS))
-  
-  x <- splitAsList(rep(factor("ambiguous"), n), seq_len(n))
-  mergeAtomicLists(x, a)
-  
-  fpr_fl_n1 <- c(as.character(fpr_fl_s_o[[1]][1]), "Unassigned")
-  fpr_fl_n1 <- FactorList(rep(list(fpr_fl_n1), length(fpr_fl_n)))
-  
-  fpr_fl_n_o <- mergeAtomicLists(fpr_fl_n1[1:10000], fpr_fl_n[1:10000])
+  fpr_fl_n_o <- mergeAtomicLists(x, fpr_fl_n)
   
   # Features for the tree
-  fpr_tree <- c(fpr_fl_s_o, fpr_fl_m_of)
+  fpr_tree <- c(fpr_fl_s_o, fpr_fl_m_of, fpr_fl_n_o)
 
   tree <- fList2tree(fL = fpr_tree, addRoot = FALSE, collapseSingles = F)
 
