be5d9b998702164ca25608329f815e1a3a208037
Deepak Tanwar <dktanwar@hotmail.com>
2021-10-14 12:28:32 +0200

    updated function to handle multiple chromosomes

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -23,29 +23,29 @@ repSoftClip <- function(GA) {
     st <- strand(gr)
     st <- as.character(st@values)
     seq <- as.character(gr$seq)
-    
+
     if (st == "-") seq <- as.character(reverseComplement(DNAStringSet(seq)))
-    
+
     seqCigar <- as.character(cigarToRleList(cigar)[[1]])
     nr <- seqCigar
     seqS <- strsplit(seq, "")[[1]]
-    
+
     if (st == "-") seqS <- rev(seqS)
-    
+
     if (length(seqCigar) != length(seqS)) {
       stop("Please provide corect sequence and matching cigar.")
     }
-    
+
     wh <- which(seqCigar == "M")
     seqCigar[wh] <- seqS[wh]
-    
+
     # if (st == "-") seqCigar <- rev(seqCigar)
-    
+
     gr$seqC <- paste(seqCigar, collapse = "")
     gr$seqN <- gsub(pattern = "S", replacement = "", x = gr$seqC)
-    
+
     nr[nr == "M"] <- start(gr):end(gr)
-    
+
     wh1 <- which(nr != "S")
     wh1 <- wh[c(1, length(wh1))]
     se <- (start(gr) - (wh[1] - 1)):(end(gr) + (length(nr) - wh1[2]))
@@ -53,7 +53,7 @@ repSoftClip <- function(GA) {
     end(gr) <- se[length(se)]
     return(gr)
   })
-  
+
   res <- GRanges(plyr::ldply(lapply(res, data.frame)))
   res
 }
@@ -71,13 +71,12 @@ repSoftClip <- function(GA) {
 #' @export
 #'
 #' @examples
-makeTracks <- function(gr, 
-                       counts, 
-                       features, 
-                       bamFile = NULL, 
+makeTracks <- function(gr,
+                       counts,
+                       features,
+                       bamFile = NULL,
                        param = ScanBamParam(what = c("cigar", "pos", "seq")),
                        plotCoverage = TRUE) {
-  
   suppressPackageStartupMessages({
     library(Rsamtools)
     library(ggplot2)
@@ -90,22 +89,23 @@ makeTracks <- function(gr,
     library(rcartocolor)
     library(Biostrings)
   })
-  
+
   ga <- readGAlignments(bamFile, param = param)
   names(ga) <- ga@elementMetadata$seq
   seqlevelsStyle(ga) <- "Ensembl"
-  
+
   if (!is.null(bamFile)) bamFile <- BamFile(bamfile)
+
   gr <- suppressWarnings(
     subsetByOverlaps(features, reduce(gr), minoverlap = 1)
   )
   gar <- suppressWarnings(
     subsetByOverlaps(ga, gr)
   )
-  
+
   mt <- data.frame(strand = gar@strand, gar@elementMetadata)
   mt$Counts <- NA
-  
+
   for (i in 1:nrow(mt)) {
     if (mt$strand[i] == "+") {
       mt$Counts[i] <- mean(as.numeric(counts[mt$seq[i], ]))
@@ -117,97 +117,108 @@ makeTracks <- function(gr,
       )
     }
   }
-  
-  
+
   gar@elementMetadata <- DataFrame(mt[, -1])
-  
+
   gr <- repSoftClip(gar)
-  
-  qr <- suppressWarnings(
-    subsetByOverlaps(features, reduce(gr))
-  )
-  qr$model <- "cds"
-  names(qr) <- qr$transcript_id
-  qrl <- as(qr, "GRangesList")
-  
-  # Make row for every letter
-  df <- mapply(function(s, e, seq, i) {
-    data.frame(
-      pos = seq(s, e, by = 1L),
-      seq = seq,
-      id = i
+
+  chrs <- unique(seqnames(gr))
+
+  garN <- gar
+  grN <- gr
+
+  res <- sapply(seq_along(chrs), function(x) {
+    gr <- grN[seqnames(grN) == chrs[x]]
+    gar <- garN[seqnames(garN) == chrs[x]]
+
+    qr <- suppressWarnings(
+      subsetByOverlaps(features, reduce(gr))
     )
-  },
-  s = start(gr), e = start(gr) + nchar(gr$seq) - 1, seq = strsplit(gr$seqC, ""), i = seq_along(gr),
-  SIMPLIFY = FALSE
-  )
-  
-  # Combine different granges
-  df <- do.call(rbind, df)
-  
-  # Determine y-position in plot
-  offset <- disjointBins(gr)
-  df$offset <- offset[df$id]
-  
-  df$seq <- factor(df$seq, levels = c("A", "T", "G", "C", "S"))
-  
-  # Plot
-  cl <- c(RColorBrewer::brewer.pal(n = 9, name = "Set1")[c(1:4, 9)])
-  names(cl) <- c("A", "T", "G", "C", "S")
-  
-  p1 <- autoplot(GRanges(gar),
-                 geom = "arrowrect", which = qr,
-                 aes(fill = Counts)
-  ) +
-    theme_linedraw()
-  # p2 <- autoplot(gar, geom = "line", stat = "coverage", size = 1.5) +
-  #   scale_color_manual(name = "Coverage", values = "black") + theme_linedraw()
-  # p2
-  
-  p2 <- ggplot(df, aes(pos, offset, fill = seq)) +
-    geom_tile(height = 0.8) +
-    scale_fill_manual(values = cl) +
-    theme_linedraw()
-  
-  if(plotCoverage){
-    wh <- reduce(gr)
-    seqlevelsStyle(wh) <- "UCSC"
-    p3 <- suppressMessages(
-      autoplot(bamFile, which = wh, size = 1, color = "black") + theme_linedraw() +
-        scale_color_identity(guide = "legend")
-    )  
-  }
-  
-  
-  # p4 <- ggplot() +
-  #   geom_alignment(qrl, type = "model", aes(fill = transcript_type),
-  #                  range.geom = "arrowrect", gap.geom = "chevron",
-  #                  names.expr = "genenames:transcript_id", columns = c("genenames, transcript_id")) +
-  #   scale_fill_carto_d(palette = "Safe") +
-  #   theme_linedraw()
-  #
-  # p5 <- ggplot() +
-  #   geom_alignment(qr, type = "model", aes(fill = transcript_type),
-  #                  range.geom = "arrowrect", group.selfish = FALSE,
-  #                  gap.geom = "chevron",
-  #                  names.expr = "transcript_id") +
-  #   scale_fill_carto_d(palette = "Safe") +
-  #   theme_linedraw()
-  
-  p6 <- suppressMessages(
-    autoplot(qrl, aes(type = model, fill = transcript_type, vjust = 0.1),
-             range.geom = "arrowrect", label.color = "black",
-             label.size = 3, hjust = 0.5, label.pos = "bottom"
+    qr$model <- "cds"
+    names(qr) <- qr$transcript_id
+    qrl <- as(qr, "GRangesList")
+
+    # Make row for every letter
+    df <- mapply(function(s, e, seq, i) {
+      data.frame(
+        pos = seq(s, e, by = 1L),
+        seq = seq,
+        id = i
+      )
+    },
+    s = start(gr), e = start(gr) + nchar(gr$seq) - 1, seq = strsplit(gr$seqC, ""), i = seq_along(gr),
+    SIMPLIFY = FALSE
+    )
+
+    # Combine different granges
+    df <- do.call(rbind, df)
+
+    # Determine y-position in plot
+    offset <- disjointBins(gr)
+    df$offset <- offset[df$id]
+
+    df$seq <- factor(df$seq, levels = c("A", "T", "G", "C", "S"))
+
+    # Plot
+    cl <- c(RColorBrewer::brewer.pal(n = 9, name = "Set1")[c(1:4, 9)])
+    names(cl) <- c("A", "T", "G", "C", "S")
+
+    p1 <- autoplot(GRanges(gar),
+      geom = "arrowrect", which = qr,
+      aes(fill = Counts)
     ) +
-      scale_fill_carto_d(palette = "Safe") +
       theme_linedraw()
-  )
-  
-  if(plotCoverage){
-    tr <- tracks(p1, p2, p3, p6, heights = c(2, 2, 2, 2)) + ylab("")  
-  } else{
-    tr <- tracks(p1, p2, p6, heights = c(2, 2, 2)) + ylab("")  
-  }
-  
-  return(tr)
+    # p2 <- autoplot(gar, geom = "line", stat = "coverage", size = 1.5) +
+    #   scale_color_manual(name = "Coverage", values = "black") + theme_linedraw()
+    # p2
+
+    p2 <- ggplot(df, aes(pos, offset, fill = seq)) +
+      geom_tile(height = 0.8) +
+      scale_fill_manual(values = cl) +
+      theme_linedraw()
+
+    if (plotCoverage) {
+      wh <- reduce(gr)
+      seqlevelsStyle(wh) <- "UCSC"
+      p3 <- suppressMessages(
+        autoplot(bamFile, which = wh, size = 1, color = "black") + theme_linedraw() +
+          scale_color_identity(guide = "legend")
+      )
+    }
+
+
+    # p4 <- ggplot() +
+    #   geom_alignment(qrl, type = "model", aes(fill = transcript_type),
+    #                  range.geom = "arrowrect", gap.geom = "chevron",
+    #                  names.expr = "genenames:transcript_id", columns = c("genenames, transcript_id")) +
+    #   scale_fill_carto_d(palette = "Safe") +
+    #   theme_linedraw()
+    #
+    # p5 <- ggplot() +
+    #   geom_alignment(qr, type = "model", aes(fill = transcript_type),
+    #                  range.geom = "arrowrect", group.selfish = FALSE,
+    #                  gap.geom = "chevron",
+    #                  names.expr = "transcript_id") +
+    #   scale_fill_carto_d(palette = "Safe") +
+    #   theme_linedraw()
+
+    p6 <- suppressMessages(
+      autoplot(qrl, aes(type = model, fill = transcript_type, vjust = 0.1),
+        range.geom = "arrowrect", label.color = "black",
+        label.size = 3, hjust = 0.5, label.pos = "bottom"
+      ) +
+        scale_fill_carto_d(palette = "Safe") +
+        theme_linedraw()
+    )
+
+    if (plotCoverage) {
+      tr <- tracks(p1, p2, p3, p6, heights = c(2, 2, 2, 2)) + ylab("")
+    } else {
+      tr <- tracks(p1, p2, p6, heights = c(2, 2, 2)) + ylab("")
+    }
+    return(tr)
+  })
+
+  names(res) <- paste("plot", "Chr", as.character(chrs), sep = "_")
+  return(res)
 }
